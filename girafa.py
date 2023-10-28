550a58d41a8d1e3409f441c43588df88950b42ea
Thamires <thamy.03@hotmail.com>
2023-10-28 15:49:29 -0300

    "Alterações"

--- girafa.py
+++ girafa.py
@@ -10,13 +10,34 @@ class Girafa:
         self.idade = idade
         self.cor = cor
         self.origem = origem
+        self.__fome = 100
     
     def andar(self):
-        print(self.nome, "está andando..")
+        if self.__fome > 60:
+            print(self.nome, "não pode andar porque está morrendo de fome")
+        else:
+            print(self.nome, "está andando..")
 
-    def comer(self):
-        print(self.nome, "está comendo..")
+    def comer(self, alimento):
+        lista_alimentos = ['folhas', 'frutas', 'plantas']
+
+        if alimento in lista_alimentos:
+            self.__fome -= 10
+        else:
+            print(self.nome, "não come esse tipo de coisa...")
     
+    def fome(self):
+        if self.__fome > 60:
+            print(self.nome, "Está morrendo de fome!")
+        elif self.__fome > 40:
+            print(self.nome, "Está com fome!")
+        elif self.__fome > 20:
+            print(self.nome, "Está saciada.")
+        elif self.__fome > 0:
+            print(self.nome, "Está cheia.")
+        elif self.__fome > 0:
+            print(self.nome, "Está explodindo.")
+
     def respirar(self):
         print(self.nome, "está viva..")
     
